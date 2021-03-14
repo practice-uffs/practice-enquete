7610fad93b9640215d786eaacc5ffc980ba51a1d
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-14 17:06:34 -0300

    chore: removendo resolvers de exemplo

--- server/src/api/schema/schema.ts
+++ server/src/api/schema/schema.ts
@@ -2,12 +2,10 @@ import 'graphql-import-node';
 import { GraphQLSchema } from 'graphql';
 import { buildSchemaSync } from 'type-graphql';
 import { HelloResolver } from './hello/hello.resolver';
-import { UserResolver } from './user/user.resolver';
-import { LoginResolver } from './login/login.resolver';
 import { Authenticator } from '@api/server/authenticator';
 
 // Load resolvers
 export const Schema: GraphQLSchema = buildSchemaSync({
-  resolvers: [HelloResolver, UserResolver, LoginResolver],
+  resolvers: [HelloResolver],
   authChecker: Authenticator.authChecker,
 });
