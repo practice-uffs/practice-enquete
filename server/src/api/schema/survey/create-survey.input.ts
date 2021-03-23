987f7317073d3b949d5c9235e41e817b3c3c2f72
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-23 17:55:49 -0300

    feat:  adding create survey input

---
+++ server/src/api/schema/survey/create-survey.input.ts
@@ -0,0 +1,19 @@
+import { InputType, Field } from 'type-graphql';
+import { IsInt, IsJSON, IsString, Min } from 'class-validator';
+import { CreateSurveyInputModel } from '@domain/model/survey.model';
+
+@InputType()
+export class CreateSurveyInput implements CreateSurveyInputModel {
+  @Field({ description: 'Identificador do usuário' })
+  @IsInt({ message: 'O identificador do usuário deve ser um número inteiro' })
+  @Min(0, { message: 'O identificador do usuário deve ser maior que zero' })
+  userId!: number;
+
+  @Field({ description: 'Título da enquete' })
+  @IsString({ message: 'O título precisa ser uma sequência de caracteres' })
+  title!: string;
+
+  @Field({ description: 'Perguntas da enquete' })
+  @IsJSON({ message: 'As perguntas precisam ser uma lista de objetos transformada em JSON' })
+  questions!: string;
+}
